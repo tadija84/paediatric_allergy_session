30130c1bc71f34e9f7b2c9ea17b1f54bdbe554ed
Tadija <30640354+tadija84@users.noreply.github.com>
2022-05-04 13:57:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var expertsImage=document.getElementById("expertsImage");
+expertsImage.addEventListener("click", redirectingPage);
+
+function redirectingPage(){
+    location.href = "https://eu.jotform.com/app/220581845851864/page/7";
+}
